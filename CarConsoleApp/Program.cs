6c421c31539679387b056c2fa8b018a7ff76cc81
Marvyn21 <marvynrogony@gmail.com>
2022-06-05 18:33:00 +0300

    console created

---
+++ CarConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CarClassLibrary;
+using System;
+
+namespace CarConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c = new Car();
+            Console.WriteLine("Car c is as follows " + c.Make + " " + c.Model + " " + c.Price);
+        }
+    }
+}
